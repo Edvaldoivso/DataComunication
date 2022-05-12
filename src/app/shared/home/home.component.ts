1b4eafb1a6bb7494ccf6dcad57739341fc3da19c
Edvaldo I Souza <edvaldoivosouza@ymail.com>
2022-05-12 10:21:00 -0300

    Input Angular

---
+++ src/app/shared/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+public addValue : number = 0
+
+Add(){
+  this.addValue ++
+}
+
+
+
+}
